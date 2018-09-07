9bd2eafb7f26197737a755722f23a1ebf754689d
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-08 00:13:19 +0200

    styles homepage

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,21 +2,48 @@
 
 @section('content')
 	@while(have_posts()) @php the_post() @endphp
-	<figure class="header-visual" style="background-image: url('http://placehold.it/1400x950');"></figure>
+	<figure class="header-visual" style="background-image: url('http://placehold.it/1400x950');">
+		<div class="header-visual--title">
+			<p>Iedereen <br />in beweging</p>
+		</div>
+	</figure>
 	<section class="intro">
 		<div class="container">
 			<article id="post-&lt;?php the_ID(); ?&gt;">
-				<header class="intro__article-header">
+				<header class="intro__article--header">
 					<h1>Aan de slag</h1>
 				</header>
-				<div class="intro__article-content">
+				<div class="intro__article--content">
 					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elisequat, vel illum dolore eu feugiat nulla facilisis at vero eros et Lorem ipsum dolor sit amet, consectetuer adipiscing elisequat, vel illum dolore eu feugiat eros et accumsan et iusto odio...</p>
 				</div>
 			</article>
 		</div>
+		</div>
 	</section>
+	
+	<style>
+		.intro {
+			position: relative;
+			width: 100%;
+			margin: 0 auto;
+			overflow: hidden;
+			padding: 20px;
+			text-align: center;
+		}
+		.intro:after {
+			content: '';
+			position: absolute;
+			width: 1100%; height: 1100%;
+			top: 20px; left: -500%;
+			background: rgba(255,255,255,.8);
+			transform-origin: 54% 0;
+			transform: rotate(45deg);
+			z-index: -1;
+		}
+	</style>
 	<section class="featured">
 		<div class="container">
+			
 			<article class="featured__item">
 				<figure class="featured__figure">
 					<img alt="" src="http://placehold.it/640x640">
@@ -24,6 +51,7 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -33,6 +61,7 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -42,6 +71,7 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -51,6 +81,19 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
+				</div>
+			</article>
+			
+			
+			<article class="featured__item">
+				<figure class="featured__figure">
+					<img alt="" src="http://placehold.it/640x640">
+				</figure>
+				<div class="featured__content--wrapper">
+					<h2 class="featured__content--title">Ik zoek een sport</h2>
+					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -60,15 +103,7 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
-				</div>
-			</article>
-			<article class="featured__item">
-				<figure class="featured__figure">
-					<img alt="" src="http://placehold.it/640x640">
-				</figure>
-				<div class="featured__content--wrapper">
-					<h2 class="featured__content--title">Ik zoek een sport</h2>
-					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -78,6 +113,7 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport dit is een langere titel</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
 			<article class="featured__item">
@@ -87,13 +123,15 @@
 				<div class="featured__content--wrapper">
 					<h2 class="featured__content--title">Ik zoek een sport</h2>
 					<p class="featured__content--intro">Je kunt op allerlei manieren sporten en bewegen via Breda Actief. We hebben voor ieder wat wils</p>
+					<a class="featured__content--link" href="#">Lees meer ></a>
 				</div>
 			</article>
+		
 		</div>
 	</section>
 	<section class="banner__visual">
 		<div class="container">
-			<figure class="banner__visual-figure">
+			<figure class="banner__visual--figure">
 				<img alt="" src="http://placehold.it/1400x520">
 			</figure>
 		</div>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,14 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
-  <link href="https://fonts.googleapis.com/css?family=Arvo:400,400i,700,700i|Karla:400,400i,700,700i" rel="stylesheet">
+  <script src="//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+  <script>
+      WebFont.load({
+          google: {
+              families: ['Arvo:400,400i,700,700i', 'Karla:400,400i,700,700i']
+          }
+      });
+  </script>
   
   @php wp_head() @endphp
 </head>
