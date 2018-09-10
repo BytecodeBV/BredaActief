abe84efe62c3d772f3a08554fda5ca1c1c4b9b85
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-10 14:13:32 +0200

    imported slider js

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -11,6 +11,7 @@ import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
 import aboutUs from './routes/about';
+import './routes/slider';
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
