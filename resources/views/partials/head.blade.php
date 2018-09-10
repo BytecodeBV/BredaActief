07a9ababfb1342397a57a85f5cff6161120e8060
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-10 14:22:03 +0200

    added cdn slick styles

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
   <script src="//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
   <script>
       WebFont.load({
