c9e32409672a07643565f447eb3879e4d7624e66
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-16 00:01:51 +0200

    add mobile nav menu

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/jQuery.mmenu/7.0.6/jquery.mmenu.all.css"/>
   <script src="//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
   <script>
       WebFont.load({
