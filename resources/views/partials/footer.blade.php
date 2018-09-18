19e7df50da824752efaeedf64cca9079c219de3a
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-19 00:17:13 +0200

    added footer widget

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,52 +1,5 @@
 <footer class="footer">
   <div class="center">
-    <div class="footer__col">
-      <h5>Sport en bewegen</h5>
-      <nav class="footer__nav">
-        <ul>
-          <li><a href="#">Ben jij in beweging?</a></li>
-          <li><a href="#">Aangepast in beweging</a></li>
-          <li><a href="#">Clubs in beweging</a></li>
-          <li><a href="#">Vereniging in beweging</a></li>
-          <li><a href="#">Scholen in beweging</a></li>
-        </ul>
-      </nav>
-    </div>
-    <div class="footer__col">
-      <h5>Snel naar</h5>
-      <nav class="footer__nav">
-        <ul>
-          <li><a href="#">Advies</a></li>
-          <li><a href="#">Online vacaturebank</a></li>
-          <li><a href="#">Verzekeringen</a></li>
-          <li><a href="#">Introductie cursussen</a></li>
-          <li><a href="#">Sportloket</a></li>
-          <li><a href="#">Sporten in de wijk</a></li>
-        </ul>
-      </nav>
-    </div>
-    <div class="footer__col">
-      <h5>Breda Actief</h5>
-      <nav class="footer__nav">
-        <ul>
-          <li><a href="#">Postbus 5675</a></li>
-          <li><a href="#">4801 EB Breda</a></li>
-          <li><a href="mailto:info@breda-actief.nl">info@breda-actief.nl</a></li>
-          <li><a href="#">076 523 35 55</a></li>
-        </ul>
-      </nav>
-      <nav class="social">
-        <ul>
-          <li class="facebook"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.84 41.84"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#f23030;}</style></defs><title>Middel 1</title><g id="Laag_2" data-name="Laag 2"><g id="Footer"><g id="Icoon_Facebook" data-name="Icoon Facebook"><circle class="cls-1" cx="20.92" cy="20.92" r="20.92"/><path class="cls-2" d="M24.6,21.52H21.76V31.64H17.55V21.52h-2V17.94h2V15.63a4,4,0,0,1,4.25-4.25h3.12v3.48H22.65a.85.85,0,0,0-.89,1v2.1H25Z"/></g></g></g></svg></a></li>
-          <li class="twitter"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.84 41.84"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#f23030;}</style></defs><title>Middel 2</title><g id="Laag_2" data-name="Laag 2"><g id="Footer"><g id="Icoon_twitter" data-name="Icoon twitter"><circle class="cls-1" cx="20.92" cy="20.92" r="20.92"/><path class="cls-2" d="M30.41,17.38c0,.18,0,.36,0,.54a11.82,11.82,0,0,1-18.2,10,8.22,8.22,0,0,0,1,.06,8.35,8.35,0,0,0,5.16-1.78,4.18,4.18,0,0,1-3.88-2.89,4.13,4.13,0,0,0,.78.08,4.42,4.42,0,0,0,1.1-.15A4.16,4.16,0,0,1,13,19.12v-.05a4.21,4.21,0,0,0,1.89.52,4.15,4.15,0,0,1-1.85-3.46A4.11,4.11,0,0,1,13.63,14a11.78,11.78,0,0,0,8.57,4.34,4.15,4.15,0,0,1,4.05-5.1,4.11,4.11,0,0,1,3,1.31,8,8,0,0,0,2.64-1,4.13,4.13,0,0,1-1.82,2.3,8.22,8.22,0,0,0,2.38-.65A8.46,8.46,0,0,1,30.41,17.38Z"/></g></g></g></svg></a></li>
-          <li class="instagram"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.84 41.84"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#f23030;}</style></defs><title>Middel 4</title><g id="Laag_2" data-name="Laag 2"><g id="Footer"><g id="Icoon_instagram" data-name="Icoon instagram"><circle class="cls-1" cx="20.92" cy="20.92" r="20.92"/><path class="cls-2" d="M28.58,32.77H15a5.06,5.06,0,0,1-5.05-5.05V14.12a5.06,5.06,0,0,1,5.05-5H28.58a5.05,5.05,0,0,1,5,5v13.6A5,5,0,0,1,28.58,32.77ZM15,10.7a3.43,3.43,0,0,0-3.42,3.42v13.6A3.42,3.42,0,0,0,15,31.14H28.58A3.42,3.42,0,0,0,32,27.72V14.12a3.42,3.42,0,0,0-3.41-3.42Z"/><path class="cls-2" d="M21.77,27.38a6.46,6.46,0,1,1,6.47-6.46A6.46,6.46,0,0,1,21.77,27.38Zm0-11.29a4.83,4.83,0,1,0,4.83,4.83A4.84,4.84,0,0,0,21.77,16.09Z"/><rect class="cls-2" x="26.81" y="12.41" width="3.47" height="3.47" rx="1.45" ry="1.45"/></g></g></g></svg></a></li>
-          <li class="linkedin"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.84 41.84"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#f23030;}</style></defs><title>Middel 5</title><g id="Laag_2" data-name="Laag 2"><g id="Footer"><g id="Icoon_linkedin" data-name="Icoon linkedin"><circle class="cls-1" cx="20.92" cy="20.92" r="20.92"/><path class="cls-2" d="M22.14,18.49v0l0,0Z"/><path class="cls-2" d="M15.39,29.66H11.05V16.6h4.34ZM13.22,14.81h0a2.26,2.26,0,1,1,.06-4.51,2.26,2.26,0,1,1,0,4.51ZM31.05,29.66H26.71v-7c0-1.75-.63-3-2.2-3a2.38,2.38,0,0,0-2.23,1.59,2.84,2.84,0,0,0-.14,1.06v7.29H17.79s.06-11.84,0-13.06h4.35v1.85a4.32,4.32,0,0,1,3.91-2.16c2.86,0,5,1.87,5,5.88Z"/></g></g></g></svg></a></li>
-          <li class="youtube"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 41.84 41.84"><defs><style>.cls-1{fill:#fff;}.cls-2{fill:#f23030;}</style></defs><title>Middel 6</title><g id="Laag_2" data-name="Laag 2"><g id="Footer"><g id="Icoon_youtube" data-name="Icoon youtube"><circle class="cls-1" cx="20.92" cy="20.92" r="20.92"/><path class="cls-2" d="M33.26,16.84v8.59c-.06.23-.13.46-.17.7A4.14,4.14,0,0,1,29.23,30a14.76,14.76,0,0,1-2.3.18q-6,0-12,0A13.79,13.79,0,0,1,12.75,30c-2.4-.36-3.91-1.6-4-3.88-.2-3.36-.13-6.75,0-10.13a3,3,0,0,1,2.43-2.8A6.56,6.56,0,0,1,13,12.87c5.36,0,10.73-.09,16.09,0,2.59.06,3.88,1.31,4.09,3.64A1.25,1.25,0,0,0,33.26,16.84ZM18,21.32c0,.85,0,1.71,0,2.56s.53,1.19,1.31.79q2.46-1.25,4.88-2.56c.8-.43.79-1.13,0-1.55C22.6,19.69,21,18.85,19.32,18c-.77-.39-1.26-.08-1.28.82S18,20.5,18,21.32Z"/></g></g></g></svg></a></li>
-        </ul>
-      </nav>
-    </div>
-    <div class="footer__col footer__col-last">
-      <p>Breda Actief is een onderdeel van <a href="#">Stichting Breda Actief</a></p>
-    </div>
+     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
