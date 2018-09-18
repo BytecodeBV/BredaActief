3c2c685c06dfcdefc992a984f57b2593123a5a0d
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-19 00:16:52 +0200

    made partial template for contact block

---
+++ resources/views/partials/content-contact.blade.php
@@ -0,0 +1,20 @@
+<?php
+$contact_title = get_field('contact_title', 'option');
+$contact_subtitle = get_field('contact_subtitle', 'option');
+$contact_form = get_field('contact_form', 'option');
+$contact_image = get_field('contact_image', 'option');
+?>
+
+<section class="contact">
+	<div class="contact__col--left">
+		<article class="contact__article">
+			<header class="contact__header">
+				<h2 class="contact__header--title">{{ $contact_title }}</h2>
+				<p class="contact__header--subtitle">{{ $contact_subtitle  }}</p>
+			</header>
+			{!! $contact_form !!}
+		</article>
+	</div>
+	<div class="contact__col--right" style="background-image:url('{{ $contact_image['url'] }}');">
+	</div>
+</section>
