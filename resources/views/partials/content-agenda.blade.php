49e2f348c13c171dc7c95243488f12dbf56020f1
Gaby Khoe <gaby.khoe@gmail.com>
2018-09-27 22:32:25 +0200

    add some hovers

--- resources/views/partials/content-agenda.blade.php
+++ resources/views/partials/content-agenda.blade.php
@@ -24,7 +24,7 @@ $events = new WP_Query($args_events);
 <section class="agenda">
 	<header class="agenda__header">
 		<div class="center">
-			<h1><?php echo __('Agenda', $text_domain); ?></h1>
+			<h2><?php echo __('Agenda', $text_domain); ?></h2>
 		</div>
 	</header>
 	<div class="slider">
@@ -36,17 +36,19 @@ $events = new WP_Query($args_events);
 		@endphp
 	
 		<article class="agenda__event">
-			<figure class="agenda__event--img">{!! $post_thumb !!}</figure>
-			<span class="agenda__event--date" style="background-color: {{ the_field('event_date_color') }};">{{ the_field('event_date') }}</span>
-			<div class="agenda__event--text">
-				<h2 class="agenda__event--title">{{ the_title() }}</h2>
-				<p class="agenda__event--subtitle">{{ the_field('event_subtitle')  }}</p>
-			</div>
+			<a href="{{ the_permalink() }}">
+				<figure class="agenda__event--img">{!! $post_thumb !!}</figure>
+				<span class="agenda__event--date" style="background-color: {{ the_field('event_date_color') }};">{{ the_field('event_date') }}</span>
+				<div class="agenda__event--text">
+					<h3 class="agenda__event--title">{{ the_title() }}</h3>
+					<p class="agenda__event--subtitle">{{ the_field('event_subtitle')  }}</p>
+				</div>
+			</a>
 		</article>
 		@endwhile
 	</div>
 	<div class="center">
-		<a class="show__all" href="#"><?php echo __('Bekijk alles >', $text_domain); ?></a>
+		<a class="show__all" href="#"><?php echo __('Bekijk alles', $text_domain); ?></a>
 	</div>
 </section>
 @php wp_reset_postdata() @endphp
