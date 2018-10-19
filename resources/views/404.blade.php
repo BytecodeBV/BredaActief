a43d475a37d3bb3ffc04c5cc93bb4175e95850eb
Gaby Khoe <gaby.khoe@gmail.com>
2018-10-19 21:37:37 +0200

    add 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,20 @@
+@php global $text_domain @endphp
+
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  @include('partials.header-visual')
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
+    <section id="scroll-to" class="intro">
+      <div class="center center-small">
+        <article id="post-{{ get_the_ID() }}" class="page-article">
+          <div class="intro__article--content">
+            <h1>{{ __('Pagina niet gevonden', $text_domain) }}</h1>
+            <p>{{ __('Sorry, deze pagina bestaat niet.', $text_domain) }}</p>
+          </div>
+        </article>
+      </div>
+    </section>
   @endif
 @endsection
