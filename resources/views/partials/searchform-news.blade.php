cadb0d58dd0b2e695590c58fb7376e9e782c7589
Gaby Khoe <gaby.khoe@gmail.com>
2018-10-19 22:43:39 +0200

    refactor code to blade

--- resources/views/partials/searchform-news.blade.php
+++ resources/views/partials/searchform-news.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 $action = get_permalink( get_option( 'page_for_posts' ));
 $name = 's_news';
 $id = 'search-form-news';
@@ -8,12 +8,11 @@ if( isset($_GET['s_news']) && $_GET['s_news'] !== '') {
 } else {
 	$value = '';
 }
-
-?>
+@endphp
 
 <form id="{{ $id }}" role="search" method="get" class="search-form" action="{{ $action }}">
   <div class="search-form__inner">
-    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Zoeken', 'placeholder', $text_domain ); ?>" value="<?php echo $value; ?>" name="{{ $name }}" />
-    <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', $text_domain ); ?></span></button>
+    <input type="search" class="search-field" placeholder="{!! esc_attr_x( 'Zoeken', 'placeholder', $text_domain ) !!}" value="{!! $value !!}" name="{{ $name }}" />
+    <button type="submit" class="search-submit"><span class="screen-reader-text">{!! _x( 'Search', 'submit button', $text_domain ) !!}</span></button>
   </div>
 </form>
