124ceab0c858876d2cb0bcb2424c157999014938
Gaby Khoe <gaby.khoe@gmail.com>
2018-10-19 23:08:41 +0200

    refactor code to blade

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,7 +1,7 @@
-<?php
-global $text_domain;
-$page_id = get_the_ID();
-?>
+@php
+	global $text_domain;
+	$page_id = get_the_ID();
+@endphp
 
 @extends('layouts.app')
 
@@ -10,14 +10,14 @@ $page_id = get_the_ID();
 	@include('partials.header-visual')
 	
 	@while(have_posts()) @php the_post() @endphp
-		@include('partials.content-intro')
+	@include('partials.content-intro')
 	@endwhile
 	
 	@include('partials.content-feat-blocks')
 	
 	@php
-	$banner_visual_id = get_field('banner_visual')['ID'];
-	$banner_visual = wp_get_attachment_image($banner_visual_id, 'image-banner');
+		$banner_visual_id = get_field('banner_visual')['ID'];
+		$banner_visual = wp_get_attachment_image($banner_visual_id, 'image-banner');
 	@endphp
 	
 	@if( $banner_visual_id )
@@ -30,47 +30,48 @@ $page_id = get_the_ID();
 	
 	@include('partials.content-agenda')
 	
-	<?php
-	$args_news = array(
-		'post_type' => 'post',
-		'posts_per_page' => 4,
-		'order' => 'DESC',
-		'orderby' => 'date'
-	);
+	@php
+		$args_news = array(
+			'post_type' => 'post',
+			'posts_per_page' => 4,
+			'order' => 'DESC',
+			'orderby' => 'date'
+		);
+		
+		$news = new WP_Query($args_news);
+	@endphp
 	
-	$news = new WP_Query($args_news);
-	
-	if( $news->have_posts()) :
-	?>
-	<section class="news">
-		<div class="center">
-			<header class="news__header">
-				<h2><?php echo __('Nieuws', $text_domain); ?></h2>
-			</header>
-			<div class="news__item-wrapper">
+	@if( $news->have_posts())
+		<section class="news">
+			<div class="center">
+				<header class="news__header">
+					<h2>{!! __('Nieuws', $text_domain) !!}</h2>
+				</header>
+				<div class="news__item-wrapper">
+					
+					@while( $news->have_posts()) @php($news->the_post())
+					
+						@php($trimmed = wp_trim_words( get_the_content(), 23, '...' ))
+						@php($post_thumb_id = get_post_thumbnail_id())
+						@php($post_thumb = wp_get_attachment_image($post_thumb_id, 'image-feat'))
+						
+						<article class="news__item">
+							<a href="{{ the_permalink() }}">
+								<figure class="news__item--img">{!! $post_thumb !!} </figure>
+								<div class="news__item--text">
+									<h3 class="news__item--title">{{ the_title() }}</h3>
+									<p class="news__item--intro">{{ $trimmed }}</p>
+								</div>
+							</a>
+						</article>
+					@endwhile
 				
-				<?php while( $news->have_posts()) : $news->the_post(); ?>
-				<?php
-					$trimmed = wp_trim_words( get_the_content(), 23, '...' );
-					$post_thumb_id = get_post_thumbnail_id();
-					$post_thumb = wp_get_attachment_image($post_thumb_id, 'image-feat');
-				?>
-				<article class="news__item">
-					<a href="{{ the_permalink() }}">
-						<figure class="news__item--img">{!! $post_thumb !!} </figure>
-						<div class="news__item--text">
-							<h3 class="news__item--title">{{ the_title() }}</h3>
-							<p class="news__item--intro">{{ $trimmed }}</p>
-						</div>
-					</a>
-				</article>
-				<?php endwhile; ?>
-				
+				</div>
+				<a class="show__all" href="#">{!! __('Bekijk alles', $text_domain) !!}</a>
 			</div>
-			<a class="show__all" href="#"><?php echo __('Bekijk alles', $text_domain); ?></a>
-		</div>
-	</section>
-	<?php wp_reset_postdata(); endif; ?>
+		</section>
+		@php(wp_reset_postdata())
+	@endif
 	
 	@include('partials.content-contact-option')
 
