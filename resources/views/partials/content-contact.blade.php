948c3bcedb374e8212c6ec674c40844ec0935709
Gaby Khoe <gaby.khoe@gmail.com>
2018-10-19 22:26:03 +0200

    refactor code to blade

--- resources/views/partials/content-contact.blade.php
+++ resources/views/partials/content-contact.blade.php
@@ -1,4 +1,4 @@
-<?php
+@php
 $page_id = get_the_ID();
 $contact_title = get_field('contact_title', $page_id);
 $contact_subtitle = get_field('contact_subtitle', $page_id);
@@ -8,12 +8,11 @@ $contact_image_url = wp_get_attachment_image_url($contact_image_id['ID'], 'image
 $contact_bg = get_field('contact_bg', $page_id);
 $contact_text_color = get_field('contact_text_color',$page_id);
 $contact_reverse = get_field('contact_reverse', $page_id);
+@endphp
 
-?>
-
-<section class="section-contact <?php echo ($contact_reverse[0] == 'reverse') ? 'reverse' : ''; ?>" style="background: <?php echo $contact_bg; ?>">
+<section class="section-contact {{ $contact_reverse[0] == 'reverse' ? 'reverse' : '' }}" style="background: {!! $contact_bg !!}">
 	<div class="contact__col--left">
-		<article class="contact__article" style="color: <?php echo $contact_text_color; ?>">
+		<article class="contact__article" style="color: {!! $contact_text_color !!}">
 			<header class="contact__header">
 				<h2 class="contact__header--title">{!! $contact_title !!}</h2>
 				<div class="contact__header--subtitle">{!! $contact_subtitle !!}</div>
