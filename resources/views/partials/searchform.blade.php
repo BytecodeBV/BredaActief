336edfdafd3932aefcc160fbe6d0681d57e2bad2
Gaby Khoe <gaby.khoe@gmail.com>
2018-10-19 22:42:06 +0200

    refactor code to blade

--- resources/views/partials/searchform.blade.php
+++ resources/views/partials/searchform.blade.php
@@ -1,13 +1,13 @@
-<?php
+@php
 $action = '/';
 $name = 's';
 $id = 'search-form';
 $value = get_search_query();
-?>
+@endphp
 
 <form id="{{ $id }}" role="search" method="get" class="search-form" action="{{ $action }}">
   <div class="search-form__inner">
-    <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Zoeken', 'placeholder', $text_domain ); ?>" value="<?php echo $value; ?>" name="{{ $name }}" />
-    <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', $text_domain ); ?></span></button>
+    <input type="search" class="search-field" placeholder="{!! esc_attr_x( 'Zoeken', 'placeholder', $text_domain ) !!}" value="{!! $value !!}" name="{{ $name }}" />
+    <button type="submit" class="search-submit"><span class="screen-reader-text">{!! _x( 'Search', 'submit button', $text_domain ) !!}</span></button>
   </div>
 </form>
