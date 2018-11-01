512ae3c609643350ee38a22da99122bd5a7f52b7
Gaby Khoe <gaby.khoe@gmail.com>
2018-11-01 21:21:14 +0100

    footer fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,21 @@
 <!doctype html>
 <html {{ get_language_attributes() }}>
-  @include('partials.head')
-  <body @php body_class() @endphp>
-    <div class="content">
-      @php do_action('get_header') @endphp
-      @include('partials.header')
-      <main class="main">
-        @yield('content')
-      </main>
-      @if (App\display_sidebar())
-        <aside class="sidebar">
-          @include('partials.sidebar')
-        </aside>
-      @endif
-    </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
-  </body>
+@include('partials.head')
+<body @php body_class() @endphp>
+<div class="content">
+  @php do_action('get_header') @endphp
+  @include('partials.header')
+  <main class="main">
+    @yield('content')
+  </main>
+  @if (App\display_sidebar())
+    <aside class="sidebar">
+      @include('partials.sidebar')
+    </aside>
+  @endif
+  @php do_action('get_footer') @endphp
+  @include('partials.footer')
+  @php wp_footer() @endphp
+</div>
+</body>
 </html>
