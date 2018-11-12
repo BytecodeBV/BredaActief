732d5e5309bdbbe9fff03eafee2898221709fecb
Gaby Khoe <gaby.khoe@gmail.com>
2018-11-12 21:49:02 +0100

    add tabs

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -173,5 +173,23 @@ export default {
         $(this).parents('.employee__slide').toggleClass('open');
       });
     });
+
+    /**
+     * Tabs
+     */
+
+    $('.tabs .tab-links a').on('click', function(e) {
+      var currentAttrValue = jQuery(this).attr('href');
+
+      // Show/Hide Tabs
+      $('.tabs ' + currentAttrValue).show().siblings().hide();
+
+      // Change/remove current tab to active
+      $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+      e.preventDefault();
+    });
+
+
   },
 };
