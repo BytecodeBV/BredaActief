d89e74c2669bdad438ab988b22b2bdeb6ca7de14
Gaby Khoe <gaby.khoe@gmail.com>
2018-11-19 22:25:04 +0100

    add init slick logo slider

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -34,6 +34,50 @@ export default {
     });
 
     /**
+     * Footer Logo Slider
+     */
+
+    $('.logo__slider').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+    /**
      * Agenda slider
      */
 
